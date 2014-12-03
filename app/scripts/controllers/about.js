cb45bc8486183e306a319ec9ff3fa49954e2d9a8
Matias Olivera <moliverafreire@gmail.com>
2014-12-03 00:06:59 -0200

    Generate AngularJS app structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name intelliroadsAngularjsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the intelliroadsAngularjsApp
+ */
+angular.module('intelliroadsAngularjsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name intelliroadsAngularjsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the intelliroadsAngularjsApp
+ */
+angular.module('intelliroadsAngularjsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
